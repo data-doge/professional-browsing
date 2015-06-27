4857911d3267265e076dbdb2f2ae28f13c581ee1
euglazer <eugenestapler@gmail.com>
2015-06-27 16:54:39 +1200

    refactor to one event handler

--- popup.js
+++ popup.js
@@ -1,17 +1,11 @@
-$(document).on('click', '#diglett', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {operation: "diglett"})
-  });
-});
+$(document).on('click', 'a', function (e) {
+  e.preventDefault();
+  var operation = $(e.target).attr('id');
+  trigger(operation);
+})
 
-$(document).on('click', '#rotator', function() {
+function trigger (operation) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {operation: "rotator"})
+    chrome.tabs.sendMessage(tabs[0].id, {operation: operation})
   });
-});
-
-$(document).on('click', '#scrambler', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {operation: "scrambler"})
-  });
-});
+}
