15e708101f6c660eefb52e1f3452006f86336206
euglazer <eugenestapler@gmail.com>
2015-06-27 17:36:56 +1200

    diglett fuckdown fxn

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ chrome.extension.onMessage.addListener(function(request) {
 
   switch (operation) {
     case 'diglett':
-      alert('diglett');
+      initiateDiglettFuckdown();
       break;
     case 'rotator':
       alert('rotator');
@@ -16,3 +16,20 @@ chrome.extension.onMessage.addListener(function(request) {
 
 
 });
+
+function initiateDiglettFuckdown () {
+
+  var diglettUrl = "https://fc06.deviantart.net/fs70/f/2010/165/9/4/050___Diglett_by_cammarin.png";
+
+  $('img').attr('src', diglettUrl);
+  $('a').text('DIGLETT');
+
+  $('*').each(function(i, element) {
+    var width = $(element).width();
+    $(element).css({
+      'backgroundImage' : 'url(' + diglettUrl + ')',
+      'backgroundSize' : width / 10
+    });
+  });
+
+}
--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ $(document).on('click', 'a', function (e) {
   e.preventDefault();
   var operation = $(e.target).attr('id');
   trigger(operation);
-})
+});
 
 function trigger (operation) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
